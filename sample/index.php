31c41bdd07f03e0c68c29e86724fba6cbeed8be1
wawa <wawa@wawas-mbp>
2016-04-04 09:49:54 +0800

    First HTML commint

---
+++ sample/index.php
@@ -0,0 +1,214 @@
+<?php include('header.php'); ?>
+
+<div class="home-features">
+    <div class="container">
+        <div id="feature-top" class="row">
+            <div class="col-md-4">
+                <div id="widget-search" class="widget widget-search">
+                    <div class="search-row">
+                        <h3>Search <span>New Automatic Cars</span></h3>
+                        <div class="form-group row">
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option>Make</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option>Model</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <button class="form-control">Search</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="search-row">
+                        <h3>Search <span>Used Automatic Cars</span></h3>
+                        <div class="form-group row">
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option>Make</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option>Model</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <button class="form-control">Search</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div id="slide-list" class="carousel carousel-fade slide" data-ride="carousel">
+
+                    <div class="slide-bulletz">
+                        <ol class="carousel-indicators slide-indicators">
+                            <li data-target="#slide-list" data-slide-to="0" class="active"></li>
+                            <li data-target="#slide-list" data-slide-to="1"></li>
+                            <li data-target="#slide-list" data-slide-to="2"></li>
+                        </ol>                            
+                    </div>
+
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+
+                            <div class="slide-image">
+                                <img src="assets/img/slide.png" />
+                            </div>
+                            <div class="slide-content">
+                                <h3>Freebie tourist and travell icon set(100 icon, png, svg)</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                                <a href="#" class="read-more">read more</a>
+                            </div>
+
+                        </div>
+                        <div class="item">
+                            <div class="slide-image">
+                                <img src="assets/img/slide.png" />
+                            </div>
+                            <div class="slide-content">
+                                <h3>Freebie tourist and travell icon set(100 icon, png, svg)</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                                <a href="#" class="read-more">read more</a>
+                            </div>
+
+                        </div>
+                        <div class="item">
+                            <div class="slide-image">
+                                <img src="assets/img/slide.png" />
+                            </div>
+                            <div class="slide-content">
+                                <h3>Freebie tourist and travell icon set(100 icon, png, svg)</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                                <a href="#" class="read-more">read more</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="top-listings" id="top-listings">
+            <h3 class="slide-label"><span>India's</span> first cars portal dedicated to <span>automatic cars</span></h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="item">
+                        <img src="assets/img/slide2.png" alt="" class="img-responsive">
+                        <h3><a href="">Ferrari new models</a></h3>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="item">
+                        <img src="assets/img/slide2.png" alt="" class="img-responsive">
+                        <h3><a href="">Ferrari new models</a></h3>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="item">
+                        <img src="assets/img/slide2.png" alt="" class="img-responsive">
+                        <h3><a href="">Ferrari new models</a></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="main-area">
+    <div class="container">
+        <div class="wide-banner">
+            <img src="assets/img/banner.png" alt="" />
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="info-list">
+                    <div class="info-image">
+                        <img src="assets/img/list.png" />
+                        <p class="meta"><span class="by">By<span class="author">Aditya</span></span><span class="on">on<span class="date">24 jan 2016</span><span class="comments"><span class="count">(4)</span> Comments</span></p>
+                    </div>
+                    <div class="info-text">
+                        <h3>Freebie tourist and travell icon set(100 icon, png, svg)</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                        <a href="#" class="read-more">Read more</a>
+                    </div>
+                </div>
+                <div class="info-list">
+                    <div class="info-image">
+                        <img src="assets/img/list.png" />
+                        <p class="meta"><span class="by">By<span class="author">Aditya</span></span><span class="on">on<span class="date">24 jan 2016</span><span class="comments"><span class="count">(4)</span> Comments</span></p>
+                    </div>
+                    <div class="info-text">
+                        <h3>Freebie tourist and travell icon set(100 icon, png, svg)</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                        <a href="#" class="read-more">Read more</a>
+                    </div>
+                </div>
+                <div class="info-list">
+                    <div class="info-image">
+                        <img src="assets/img/list.png" />
+                        <p class="meta"><span class="by">By<span class="author">Aditya</span></span><span class="on">on<span class="date">24 jan 2016</span><span class="comments"><span class="count">(4)</span> Comments</span></p>
+                    </div>
+                    <div class="info-text">
+                        <h3>Freebie tourist and travell icon set(100 icon, png, svg)</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                        <a href="#" class="read-more">Read more</a>
+                    </div>
+                </div>
+                <div class="info-list">
+                    <div class="info-image">
+                        <img src="assets/img/list.png" />
+                        <p class="meta"><span class="by">By<span class="author">Aditya</span></span><span class="on">on<span class="date">24 jan 2016</span><span class="comments"><span class="count">(4)</span> Comments</span></p>
+                    </div>
+                    <div class="info-text">
+                        <h3>Freebie tourist and travell icon set(100 icon, png, svg)</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
+                        <a href="#" class="read-more">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="widget widget-sidebar">
+                    <div class="widget-title">
+                        <h3>Latest News</h3>
+                    </div>
+                    <div class="widget-content">
+                        <ul class="news">
+                            <li><a href="#">Something news title here bla bla</a></li>
+                            <li><a href="#">Something news title here bla bla</a></li>
+                            <li><a href="#">Something news title here bla bla</a></li>
+                            <li><a href="#">Something news title here bla bla</a></li>
+                            <li><a href="#">Something news title here bla bla</a></li>
+                            <li><a href="#">Something news title here bla bla</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="widget widget-ads">
+                    <img src="assets/img/banner.jpg" />
+                </div>
+                <div class="widget  widget-sidebar">
+                    <div class="widget-title">
+                        <h3>Facebook</h3>
+                    </div>
+                    <div class="widget-content">
+                        <img src="assets/img/fb.png" />
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="wide-banner">
+            <img src="assets/img/banner.png" alt="" />
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>
